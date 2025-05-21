07dd6aa184b73656cae0fba16de9f191d870a33b
Seb-Woo <seb20037estudy@gmail.com>
2025-05-21 15:23:12 -0600

    initial_commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//npm install express
+//npm install cors
+//npm init -y
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10 °C", timestamp: new Date().toISOString() });
+});
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
